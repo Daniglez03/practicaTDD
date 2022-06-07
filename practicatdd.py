1cbd8ab576d441c768ea3b35101088e59a5dcc0b
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-06-07 10:31:49 +0100

    Featured: Commit inicial test1

---
+++ practicatdd.py
@@ -0,0 +1,13 @@
+class Contador():
+    def __init__(self, inicial, incremento, limite):
+        self.__inicio = inicial
+        self.__incremento = incremento
+        self.__limite = limite
+
+
+
+
+
+#https://www.mclibre.org/consultar/python/lecciones/python-for.html
+#https://es.stackoverflow.com/questions/384297/cómo-puedo-validar-si-un-input-es-vacío-en-python
+
---
+++ test.py
@@ -0,0 +1,17 @@
+from typing_extensions import self
+import unittest
+
+from practicatdd import Contador
+
+class Test_contador(unittest.TestCase):
+        #Primer test
+    def test1(self):
+        # condicion inicial
+        contador1 = Contador.incrementar(0, 2, 5)
+        # verificar
+        self.assertEqual(contador1.inicial, 0)
+        self.assertEqual(contador1.incremento, 2)
+        self.assertEqual(contador1.limite, 5)
+
+if __name__=="__main__":
+    unittest.main()
