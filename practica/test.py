a550471c4cf03e4585b95565ef19bbf76619e0ab
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-06-07 11:16:39 +0100

    Commit erroneo test2 (rojo)

--- practica/test.py
+++ practica/test.py
@@ -10,6 +10,15 @@ class Test_contador(unittest.TestCase):
         self.assertEqual(contador1.inicio, 0)
         self.assertEqual(contador1.incremento, 2)
         self.assertEqual(contador1.limite, 5)
+    
+    def test2(self):
+        #Segundo test
+        variable = 3
+        contador2 = Contador()
+        self.assertEqual(contador2.inicio, )
+        self.assertEqual(contador2.incremento, )
+        self.assertEqual(contador2.limite, variable)
+
 
 if __name__=="__main__":
     unittest.main()
