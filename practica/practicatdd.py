f68740390f1c39cfeb7353e7705bb0d6ca29650d
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-06-07 10:59:14 +0100

    Commit test erroneo (rojo)

---
+++ practica/practicatdd.py
@@ -0,0 +1,5 @@
+class Contador():
+    def __init__(self, inicial, incremento, limite):
+        self.inicio = inicial
+        self.incremento = incremento
+        self.limite = limite
--- practica/test.py
+++ practica/test.py
@@ -1,13 +1,11 @@
-from typing_extensions import self
 import unittest
-
 from practicatdd import Contador
 
 class Test_contador(unittest.TestCase):
         #Primer test
     def test1(self):
         # condicion inicial
-        contador1 = Contador(0, 2, 5)
+        contador1 = Contador.incrementar(0, 2, 5)
         # verificar
         self.assertEqual(contador1.inicial, 0)
         self.assertEqual(contador1.incremento, 2)
