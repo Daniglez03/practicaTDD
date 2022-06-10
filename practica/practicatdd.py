e4f04efb66e6eeabeb34bbf72da800c1fde40899
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-06-10 10:24:05 +0100

    Commit erroneo test3 (rojo)

--- practica/practicatdd.py
+++ practica/practicatdd.py
@@ -3,3 +3,9 @@ class Contador():
         self.inicio = inicial
         self.incremento = incremento
         self.limite = limite
+    def __inicio__(self):
+        return self.inicio
+    def __incremento__(self):
+        return self.incremento
+    def __limite__(self):
+        return self.limite
--- practica/test.py
+++ practica/test.py
@@ -10,6 +10,20 @@ class Test_contador(unittest.TestCase):
         self.assertEqual(contador1.inicio, 0)
         self.assertEqual(contador1.incremento, 2)
         self.assertEqual(contador1.limite, 5)
+    def test2(self):
+        #Segundo test
+        variable = 3
+        contador2 = Contador()
+        self.assertEqual(contador2.inicio, 0)
+        self.assertEqual(contador2.incremento, 1)
+        self.assertEqual(contador2.limite, variable)
+    def test3(self):
+        #Tercer test
+        contador3 = Contador(inicial=0, incremento=2, limite=4)
+
+        self.assertEqual(contador3.inicio)
+        self.assertEqual(contador3.incremento)
+        self.assertEqual(contador3.limite)
 
 if __name__=="__main__":
     unittest.main()
